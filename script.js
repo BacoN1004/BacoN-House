3ccdec6d9f44aae17ba382708e664cec00959e9f
BacoN1004 <101983525+BacoN1004@users.noreply.github.com>
2022-10-09 17:09:28 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var youtube = function(){
+    window.location = "https://www.youtube.com/channel/UCu3zHenNRwvMGTf_I9BaBYw";
+}
+var instagram = function(){
+    window.location = "https://www.instagram.com/bacon_quello_su_ig/";
+}
+var github = function(){
+    window.location = "https://github.com/BacoN1004";
+}
